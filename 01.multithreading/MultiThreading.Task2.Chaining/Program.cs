e3a0663e1cb810fee1b5a957c4c6889e59668cb4
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-11-16 18:36:59 +0300

    Task2

--- 01.multithreading/MultiThreading.Task2.Chaining/Program.cs
+++ 01.multithreading/MultiThreading.Task2.Chaining/Program.cs
@@ -6,6 +6,8 @@
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task2.Chaining
 {
@@ -21,9 +23,53 @@ namespace MultiThreading.Task2.Chaining
             Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
             Console.WriteLine();
 
-            // feel free to add your code
+            Random random = new Random();
+
+            Task<Int32[]> arrayTask = Task.Run(() => {
+                return generateArray(10, random);
+            });
+
+            Task<Int32[]> multiplyTask = arrayTask.ContinueWith<Int32[]>((antecedent) =>
+            {
+                int rndInt = getRandomInteger(random);
+                return antecedent.Result.Select(x => x * rndInt).ToArray();
+            });
+
+            Task<Int32[]> sortTask = multiplyTask.ContinueWith<Int32[]>((antecedent) =>
+            {
+                Array.Sort(antecedent.Result);
+                return antecedent.Result;
+            });
+
+            Task<Double> avgTask = sortTask.ContinueWith<Double>((antecedent) =>
+            {
+                return antecedent.Result.Average();
+            });
+
+            Console.WriteLine("Random Array: [{0}]", string.Join(", ", arrayTask.Result));
+
+            Console.WriteLine("Multiplied Array: [{0}]", string.Join(", ", multiplyTask.Result));
+
+            Console.WriteLine("Sorted Array: [{0}]", string.Join(", ", sortTask.Result));
+
+            Console.WriteLine("Avg: {0}", avgTask.Result);
 
             Console.ReadLine();
+        }
+
+        public static int[] generateArray(int count, Random random)
+        {
+            int[] values = new int[count];
+
+            for (int i = 0; i < count; ++i)
+                values[i] = getRandomInteger(random);
+
+            return values;
+        }
+
+        public static int getRandomInteger(Random random)
+        {
+            return random.Next();
         }
     }
 }
