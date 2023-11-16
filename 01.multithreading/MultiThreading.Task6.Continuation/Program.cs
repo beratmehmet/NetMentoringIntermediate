478bc4095bf0dd13b617788c929066198228c0b7
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-11-16 18:40:05 +0300

    Task6

--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -22,9 +24,46 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var tokenSource2 = new CancellationTokenSource();
+            CancellationToken ct = tokenSource2.Token;
 
-            Console.ReadLine();
+            while (true)
+            {
+                Task antecedent = Task.Run(() =>
+                {
+                    Console.WriteLine("What is the expected behaviour of the antecedent?");
+                    Console.WriteLine("[1]Ran to completion [2]Faulted [3]Cancelled");
+                    int input = Convert.ToInt32(Console.ReadLine());
+
+                    if (input == 1)
+                    {
+                        Console.WriteLine("Task Completed Successfully");
+                    }
+                    else if (input == 2)
+                    {
+                        Console.WriteLine("Task Faulted");
+                        throw new Exception("Task Faulted");
+                    }
+                    else if (input == 3)
+                    {
+                        Console.WriteLine("Task Cancelled");
+                        tokenSource2.Cancel();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input!!!");
+                    }
+                    ct.ThrowIfCancellationRequested();
+                }, tokenSource2.Token);
+
+                Task continuation1 = antecedent.ContinueWith(t => Console.WriteLine("Continuation-1 Executed"), TaskContinuationOptions.None);
+                Task continuation2 = antecedent.ContinueWith(t => Console.WriteLine("Continuation-2 Executed: Antecedent NotRanToCompletion"), TaskContinuationOptions.NotOnRanToCompletion);
+                Task continuation3 = antecedent.ContinueWith(t => Console.WriteLine("Continuation-3 Executed: Antecedent Faulted"), TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+                Task continuation4 = antecedent.ContinueWith(t => Console.WriteLine("Continuation-4 Executed: Antecedent Canceled"), TaskContinuationOptions.OnlyOnCanceled);
+
+                 antecedent.Wait();
+                
+            }
         }
     }
 }
