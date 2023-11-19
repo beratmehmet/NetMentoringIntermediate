ef7f2eccdb5c8cb14480b75938e4f4d5c21c9023
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-11-20 02:14:32 +0300

    Task1

--- AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
+++ AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -5,16 +5,23 @@ namespace AsyncAwait.Task1.CancellationTokens;
 internal static class Calculator
 {
     // todo: change this method to support cancellation token
-    public static long Calculate(int n /*, CancellationToken token*/)
+    public static long Calculate(int n, CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         long sum = 0;
 
         for (var i = 0; i < n; i++)
         {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+            }
             // i + 1 is to allow 2147483647 (Max(Int32)) 
             sum = sum + (i + 1);
             Thread.Sleep(10);
         }
+        
 
         return sum;
     }
