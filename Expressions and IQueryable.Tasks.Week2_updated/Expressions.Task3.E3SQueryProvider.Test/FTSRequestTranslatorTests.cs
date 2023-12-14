c02c84fafca7b80b141353a65b8ed0f9a37a37f8
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-12-14 03:49:16 +0300

    Update FTSRequestTranslatorTests.cs

--- Expressions and IQueryable.Tasks.Week2_updated/Expressions.Task3.E3SQueryProvider.Test/FTSRequestTranslatorTests.cs
+++ Expressions and IQueryable.Tasks.Week2_updated/Expressions.Task3.E3SQueryProvider.Test/FTSRequestTranslatorTests.cs
@@ -9,15 +9,19 @@ namespace Expressions.Task3.E3SQueryProvider.Test
     public class FtsRequestTranslatorTests
     {
         #region SubTask 1 : operands order
+        private readonly ExpressionToFtsRequestTranslator _translator;
+        public FtsRequestTranslatorTests()
+        {
+            _translator = new ExpressionToFtsRequestTranslator();
+        }
 
         [Fact]
         public void TestBinaryBackOrder()
         {
-            var translator = new ExpressionToFtsRequestTranslator();
             Expression<Func<EmployeeEntity, bool>> expression
                 = employee => "EPRUIZHW006" == employee.Workstation;
 
-            string translated = translator.Translate(expression);
+            string translated = _translator.Translate(expression);
             Assert.Equal("Workstation:(EPRUIZHW006)", translated);
         }
 
@@ -28,66 +32,60 @@ namespace Expressions.Task3.E3SQueryProvider.Test
         [Fact]
         public void TestBinaryEqualsQueryable()
         {
-            var translator = new ExpressionToFtsRequestTranslator();
             Expression<Func<IQueryable<EmployeeEntity>, IQueryable<EmployeeEntity>>> expression
                 = query => query.Where(e => e.Workstation == "EPRUIZHW006");
 
-            string translated = translator.Translate(expression);
+            string translated = _translator.Translate(expression);
             Assert.Equal("Workstation:(EPRUIZHW006)", translated);
         }
 
         [Fact]
         public void TestBinaryEquals()
         {
-            var translator = new ExpressionToFtsRequestTranslator();
             Expression<Func<EmployeeEntity, bool>> expression
                 = employee => employee.Workstation == "EPRUIZHW006";
 
-            string translated = translator.Translate(expression);
+            string translated = _translator.Translate(expression);
             Assert.Equal("Workstation:(EPRUIZHW006)", translated);
         }
 
         [Fact]
         public void TestMethodEquals()
         {
-            var translator = new ExpressionToFtsRequestTranslator();
             Expression<Func<EmployeeEntity, bool>> expression
                 = employee => employee.Workstation.Equals("EPRUIZHW006");
 
-            string translated = translator.Translate(expression);
+            string translated = _translator.Translate(expression);
             Assert.Equal("Workstation:(EPRUIZHW006)", translated);
         }
 
         [Fact]
         public void TestStartsWith()
         {
-            var translator = new ExpressionToFtsRequestTranslator();
             Expression<Func<EmployeeEntity, bool>> expression
                 = employee => employee.Workstation.StartsWith("EPRUIZHW006");
             
-            string translated = translator.Translate(expression);
+            string translated = _translator.Translate(expression);
             Assert.Equal("Workstation:(EPRUIZHW006*)", translated);
         }
 
         [Fact]
         public void TestEndsWith()
         {
-            var translator = new ExpressionToFtsRequestTranslator();
             Expression<Func<EmployeeEntity, bool>> expression
                 = employee => employee.Workstation.EndsWith("IZHW0060");
 
-            string translated = translator.Translate(expression);
+            string translated = _translator.Translate(expression);
             Assert.Equal("Workstation:(*IZHW0060)", translated);
         }
 
         [Fact]
         public void TestContains()
         {
-            var translator = new ExpressionToFtsRequestTranslator();
             Expression<Func<EmployeeEntity, bool>> expression
                 = employee => employee.Workstation.Contains("IZHW006");
 
-            string translated = translator.Translate(expression);
+            string translated = _translator.Translate(expression);
             Assert.Equal("Workstation:(*IZHW006*)", translated);
         }
 
